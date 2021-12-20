b7a8110447f11c786a0b4e0f4a53cdf50a7eeaaf
Aka.Kiril <vuongtuan1211@gmail.com>
2021-12-20 22:13:48 +0700

    change env

--- config/default.ts
+++ config/default.ts
@@ -1,1 +1,4 @@
-export const SOCKET_URL = "https://socket-ts.herokuapp.com/";
+export const SOCKET_URL =
+  process.env.NODE_ENV !== "production"
+    ? "http://localhost:4000"
+    : "https://socket-ts.herokuapp.com/";
